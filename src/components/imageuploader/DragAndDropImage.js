070a00cc6b33a793691d59c2cd4c56412e14d2cc
huixuant <huixuant.stu@gmail.com>
2022-05-20 11:06:00 +0800

    Add state hook to retrieve values from create survey form

--- src/components/imageuploader/DragAndDropImage.js
+++ src/components/imageuploader/DragAndDropImage.js
@@ -39,11 +39,16 @@ function DragAndDropImage() {
   }
 
   return (
-      <div onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}>
+      <div onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop} class={styles.dragArea}>
         <input ref={inputRef} type="file" id={styles.imgUpload} onChange={handleChange}/>
         <label id={styles.labelImgUpload} for="img-upload" className={isDragged ? styles.isDragged : ""}>
           <div onClick={handleClick}>
-            <p>Drag and drop the image here or click anywhere to upload.</p>
+            <div class={styles.iconParent}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class={`bi bi-file-earmark-arrow-up-fill ${styles.icon}`} viewBox="0 0 16 16">
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.354 9.854a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 8.707V12.5a.5.5 0 0 1-1 0V8.707L6.354 9.854z"/>
+              </svg>
+            </div>
+            <p class={styles.dragAreaText}>Drag and drop the image here<br></br>or click anywhere to upload.</p>
           </div> 
         </label>
       </div>
