2f46bcbd32082644c87a5604295c2ee817569847
maxtance <e0772757@u.nus.edu>
2022-06-08 17:23:13 +0800

    Implemented survey information page to view listing in more details.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 import MySurveysPage from "./components/MySurveys/MySurveysPage";
 import CreateSurvey from "./components/createSurvey/CreateSurvey";
+import SurveyInfo from "./components/SurveyInfo/SurveyInfo";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             path="/mysurveys/create-survey"
             element={<CreateSurvey />}
           ></Route>
+          <Route path="/surveys/:surveyId" element={<SurveyInfo />} />
         </Routes>
       </div>
     </Router>
--- src/components/SurveyCard/SurveyCard.js
+++ src/components/SurveyCard/SurveyCard.js
@@ -1,13 +1,23 @@
 import styles from "./SurveyCard.module.css";
+import { useNavigate } from "react-router-dom";
 
 function SurveyCard(props) {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.surveyCard}>
       <div className={styles.surveyCard__body}>
-        <h2 className={styles.surveyCard__title}>{props.title}</h2>
-        <p className={styles.surveyCard__description}>{props.description}</p>
+        <h2 className={styles.surveyCard__title}>{props.survey.title}</h2>
+        <p className={styles.surveyCard__description}>
+          {props.survey.description}
+        </p>
       </div>
-      <button className={styles.surveyCard__btn}>More information</button>
+      <button
+        className={styles.surveyCard__btn}
+        onClick={() => navigate("/surveys/" + props.survey.id)}
+      >
+        More information
+      </button>
     </div>
   );
 }
